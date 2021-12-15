6c26da9b420b3f824214b53f3658cb42c0cecf66
Grzegorz Sirocki <daddyip@wp.pl>
2021-12-15 20:06:41 +0100

    ĆW 1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,44 @@ public class Main {
     public static void main(String[] args) {
 
         /* ćw 1 enum-y */
-
+        Scanner scan = new Scanner(System.in);
+        enum LiczbyEnum {
+            JEDEN, DWA, TRZY, CZTERY, PIĘĆ, SZEŚĆ;
+        }
+        enum StatusEnum {
+            KONTYNUUJEMY, KONIEC;
+        }
+        while (true) {
+            System.out.println("Podaj liczbę:");
+            int number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            switch (scan.nextInt()) {
+                case 0:
+                    System.out.println("KONIEC");
+                    break;
+                default:
+                    System.out.println("KONTYNUUJEMY");
+                    break;
+                case 1:
+                    System.out.println("Jeden");
+                    break;
+                case 2:
+                    System.out.println("Dwa");
+                    break;
+                case 3:
+                    System.out.println("Trzy");
+                    break;
+                case 4:
+                    System.out.println("Cztery");
+                    break;
+                case 5:
+                    System.out.println("Pięć");
+                    break;
+                case 6:
+                    System.out.println("Sześć");
+                    break;
         /* zad.1
         a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
         b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
@@ -17,8 +54,10 @@ public class Main {
         g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
         */
 
-        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
-        * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
+                /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
+                 * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
+            }
+        }
     }
 }
