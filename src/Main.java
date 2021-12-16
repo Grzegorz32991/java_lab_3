0f4e6cc7e05033603e5bfad704bfabe906898eeb
Grzegorz Sirocki <daddyip@wp.pl>
2021-12-16 18:17:56 +0100

    ĆW 2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+class Student{
 
-    public static void main(String[] args) {
+        private String imie;
+        private String nazwisko;
+        private int wiek;
+        private boolean czyjestszczepiony;
+
+public Student (String imie, String nazwisko, int wiek, boolean czyjestszczepiony){
+
+    this.imie = imie;
+    this.nazwisko = nazwisko;
+    this.wiek = wiek;
+    this.czyjestszczepiony = czyjestszczepiony;
+}
+
+
+
+    String getImie(){return imie;}
+    String getNazwisko(){return nazwisko;}
+    int getWiek(){return wiek;}
+    boolean getCzyjestszczepiony(){return czyjestszczepiony;}
+
+    public String toString() {
+        return "Imię: "+imie+", Nazwisko: "+nazwisko+", Wiek: "+wiek+", Czy jest szczepiony? " +czyjestszczepiony;
+    }
+    public class Main {
+
+        public static void main(String[] args) {
+
+            Student krzysiek = new Student("Krzysiek", "Piotrowicz", 20, true);
+            Student piotrek = new Student("Piotrek", "Wolny", 30, true);
+            Student kasia = new Student("Kasia", "Krotwicka", 25, false);
+            Student wlodek = new Student("Wlodek", "Zieba", 23, true);
+
+            Student[] students = {krzysiek, piotrek, kasia, wlodek};
+
+
+        }
+        public static void wyswietl(Student[] students){
+            for(Student p: students)
+                System.out.println(p);
+        }
+
 
         /* ćw 2 klasy */
         /* zad.1
@@ -15,4 +55,6 @@ public class Main {
             d) dane powinny być estetycznie przedstawione
          */
     }
+
 }
+
